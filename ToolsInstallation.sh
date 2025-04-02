489259193123582fb8c89a6f929b66b0ff265375
vijaysai <vijaysai.chinthala45@gmail.com>
2025-04-02 11:33:06 +0530

    script file

---
+++ ToolsInstallation.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+user=$(id -u) 
+
+Red="/e[31m"
+Green="/e[32m"
+Normal="/e[0m"
+
+
+if [ $user -ne 0 ]
+then
+echo " please run the script with the root user"
+exit 1
+else 
+echo "you are the super user installation will be inprogress"
+fi
+
+validate()
+if [$1 -ne 0]
+then
+echo -e "Installation of $2 is $Red Failure.. $Normal"
+exit 1
+else
+echo -e "Installation of the $2 is $Green success..$Normal"
+fi
+
+# Install Terraform
+apt install wget -y
+wget -O- https://apt.releases.hashicorp.com/gpg |  gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" |  tee /etc/apt/sources.list.d/hashicorp.list
+apt update &&  apt install terraform -y
+validate $? "Terraform"
