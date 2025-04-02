3a98a4eea6d98c7d982d9e7fad0494b6e0789ce0
vijaysai <vijaysai.chinthala45@gmail.com>
2025-04-02 11:55:23 +0530

    script file

--- ToolsInstallation.sh
+++ ToolsInstallation.sh
@@ -24,8 +24,8 @@ echo -e "Installation of the $2 is $Green success..$Normal"
 fi
 
 # Install Terraform
-apt install wget -y
-wget -O- https://apt.releases.hashicorp.com/gpg |  gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" |  tee /etc/apt/sources.list.d/hashicorp.list
-apt update &&  apt install terraform -y
+
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
 validate $? "Terraform"
