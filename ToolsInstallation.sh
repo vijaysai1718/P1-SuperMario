ccb07c88c2f9782e825f33199d26157303cba3b0
vijaysai <vijaysai.chinthala45@gmail.com>
2025-04-02 12:02:22 +0530

    script file

--- ToolsInstallation.sh
+++ ToolsInstallation.sh
@@ -15,7 +15,7 @@ echo "you are the super user installation will be inprogress"
 fi
 
 validate()
-if [$1 -ne 0]
+if [$? -ne 0]
 then
 echo -e "Installation of $2 is $Red Failure.. $Normal"
 exit 1
@@ -27,5 +27,5 @@ fi
 
 wget -O - https://apt.releases.hashicorp.com/gpg |  gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
- apt update &&  apt install terraform
+apt update &&  apt install terraform
 validate $? "Terraform"
